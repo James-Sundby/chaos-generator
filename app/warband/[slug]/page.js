c04a34ec51ad89b3eafc31360980304531d9919e
James <119804969+James-Sundby@users.noreply.github.com>
2024-12-17 19:32:48 -0700

    Adjusts card dimensions

--- app/warband/[slug]/page.js
+++ app/warband/[slug]/page.js
@@ -8,36 +8,32 @@ export default function WarbandPage({ params }) {
 
     return (
         <div className="flex flex-1 justify-center">
-            <div id="trading-card" className="card bg-yellow-600 text-primary-content w-full max-w-96 h-3/4">
+            <div id="trading-card" className="card bg-yellow-600 text-primary-content w-full max-w-96 my-auto">
                 <div className="card-body p-2 m-2 bg-white rounded-lg">
                     <h2 className="card-title">{warbandName}</h2>
+                    <div className="flex flex-row gap-2">
+                        <Image
+                            src={patternSrc}
+                            width={300}
+                            height={450}
+                            alt={`Pattern for ${warbandName}`}
+                            className="rounded-lg"
+                        />
 
-                    <Image
-                        src={patternSrc}
-                        width={400}
-                        height={600}
-                        alt={`Pattern for ${warbandName}`}
-                        className="rounded-lg"
-                    />
-
-                    <div className="flex-1 join join-horizontal">
-                        {namedColors.map((color, index) => (
-                            <div
-                                key={index}
-                                className="w-full h-24 join-item border border-primary-content"
-                                style={{ backgroundColor: color.hex }}
-                                title={color.name}
-                            >
-                            </div>
-                        ))}
+                        <div className="flex-1 join join-vertical">
+                            {namedColors.map((color, index) => (
+                                <div
+                                    key={index}
+                                    className="w-100 h-full join-item border border-primary-content"
+                                    style={{ backgroundColor: color.hex }}
+                                    title={color.name}
+                                >
+                                </div>
+                            ))}
+                        </div>
                     </div>
                     <p className="text-sm">Colors: <span className="font-bold">{namedColors[0].name}, {namedColors[1].name}, {namedColors[2].name}</span></p>
-                    {/* <div >
-                        {namedColors.map((color, index) => (
-                            <p key={index} className="text-center text-xs">{color.name}</p>
-                        ))}
-                    </div> */}
-                    <div className="card-actions justify-end text-xs">
+                    <div className=" justify-end text-xs">
                         <p>{slug}</p>
                     </div>
                 </div>
