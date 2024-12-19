6003e78b98649e2fc2f3c470ae05adbdce1bf528
James <119804969+James-Sundby@users.noreply.github.com>
2024-12-18 18:06:26 -0700

    Adjust trading card size

--- app/components/trading-card.js
+++ app/components/trading-card.js
@@ -15,20 +15,19 @@ export default function TradingCard({ warbandName, patternSrc, namedColors, slug
         } else if (patternName === "Centered") {
             return <Centered color1={namedColors[0].hex} color2={namedColors[1].hex} />;
         } else {
-            return null; // Return null if no valid patternName is provided
+            return null;
         }
     }
 
     return (
-        <div id="trading-card" className="card bg-yellow-600 text-primary-content w-full max-w-96 h-fit">
-
+        <div id="trading-card" className="card bg-yellow-600 text-primary-content w-full max-w-80 h-fit">
             <div className="card-body p-2 m-2 bg-white rounded-lg">
-                <h2 className="card-title">{warbandName}</h2>
-                <div className="flex flex-row gap-2">
-                    <div >
-                        {ImageDisplay(patternName)}
-                    </div>
-                    <div className="flex-1 join join-vertical">
+                <h2 className="card-title justify-center">{warbandName}</h2>
+                {/* <div className="flex flex-row gap-2"> */}
+                <div className="h-96">
+                    {ImageDisplay(patternName)}
+                </div>
+                {/* <div className="flex-1 join join-vertical">
                         {namedColors.map((color, index) => (
                             <div
                                 key={index}
@@ -38,11 +37,21 @@ export default function TradingCard({ warbandName, patternSrc, namedColors, slug
                             >
                             </div>
                         ))}
-                    </div>
+                    </div> */}
+                {/* </div> */}
+                <div className="flex flex-1 join join-horizontal">
+                    {namedColors.map((color, index) => (
+                        <div
+                            key={index}
+                            className="w-full h-6 join-item border border-primary-content"
+                            style={{ backgroundColor: color.hex }}
+                            title={color.name}
+                        >
+                        </div>
+                    ))}
                 </div>
                 {/* <p className="text-sm">Pattern: <span className="font-bold">{patternName}</span></p> */}
                 <p className="text-sm">Colors: <span className="font-bold">{namedColors[0].name}, {namedColors[1].name}</span></p>
-
                 <div className=" justify-end text-xs">
                     <p>{slug}</p>
                 </div>
