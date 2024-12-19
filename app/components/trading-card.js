4482cdf46e0ed4404b115e0bec43b6e919a2c41b
James <119804969+James-Sundby@users.noreply.github.com>
2024-12-18 19:14:18 -0700

    Adds half and quarter templates
    Also removes old png files

--- app/components/trading-card.js
+++ app/components/trading-card.js
@@ -2,6 +2,8 @@ import Arms from "./model/arms";
 import Shoulders from "./model/shoulders";
 import Legs from "./model/legs";
 import Centered from "./model/centered";
+import Half from "./model/half";
+import Quarter from "./model/quarter";
 
 export default function TradingCard({ warbandName, patternSrc, namedColors, slug, patternName }) {
 
@@ -14,6 +16,10 @@ export default function TradingCard({ warbandName, patternSrc, namedColors, slug
             return <Legs color1={namedColors[0].hex} color2={namedColors[1].hex} />;
         } else if (patternName === "Centered") {
             return <Centered color1={namedColors[0].hex} color2={namedColors[1].hex} />;
+        } else if (patternName === "Half") {
+            return <Half color1={namedColors[0].hex} color2={namedColors[1].hex} />;
+        } else if (patternName === "Quarter") {
+            return <Quarter color1={namedColors[0].hex} color2={namedColors[1].hex} />;
         } else {
             return null;
         }
@@ -23,22 +29,9 @@ export default function TradingCard({ warbandName, patternSrc, namedColors, slug
         <div id="trading-card" className="card bg-yellow-600 text-primary-content w-full max-w-80 h-fit">
             <div className="card-body p-2 m-2 bg-white rounded-lg">
                 <h2 className="card-title justify-center">{warbandName}</h2>
-                {/* <div className="flex flex-row gap-2"> */}
                 <div className="h-96">
                     {ImageDisplay(patternName)}
                 </div>
-                {/* <div className="flex-1 join join-vertical">
-                        {namedColors.map((color, index) => (
-                            <div
-                                key={index}
-                                className="max-w-11 w-full min-w-6 h-full join-item border border-primary-content"
-                                style={{ backgroundColor: color.hex }}
-                                title={color.name}
-                            >
-                            </div>
-                        ))}
-                    </div> */}
-                {/* </div> */}
                 <div className="flex flex-1 join join-horizontal">
                     {namedColors.map((color, index) => (
                         <div
@@ -50,7 +43,6 @@ export default function TradingCard({ warbandName, patternSrc, namedColors, slug
                         </div>
                     ))}
                 </div>
-                {/* <p className="text-sm">Pattern: <span className="font-bold">{patternName}</span></p> */}
                 <p className="text-sm">Colors: <span className="font-bold">{namedColors[0].name}, {namedColors[1].name}</span></p>
                 <div className=" justify-end text-xs">
                     <p>{slug}</p>
