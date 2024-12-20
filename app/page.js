5458292bc69b626d3a5e1fc89524ddb29417f8fb
James <119804969+James-Sundby@users.noreply.github.com>
2024-12-19 20:42:02 -0700

    Adds test background

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import WarbandButton from "./components/warbandButton";
+import Background from "./components/background";
 
 export default function Home() {
-
   return (
-    <main className="flex flex-col flex-1 justify-center items-center px-4">
+    <main className="flex flex-col flex-1 justify-center items-center px-4 relative">
       <WarbandButton message="Generate a New Warband" />
+      <Background />
     </main>
   );
 }
