968b655736d218f15c2e94e7c0b8d88fa58878d7
James <119804969+James-Sundby@users.noreply.github.com>
2024-12-22 13:58:23 -0700

    Updates search accessibility

--- app/components/warbandSearch.js
+++ app/components/warbandSearch.js
@@ -78,9 +78,10 @@ export default function WarbandSearch() {
                     type="submit"
                     className={`btn btn-primary rounded-lg items-center justify-center ${isSubmitting ? "loading" : ""}`}
                     disabled={isSubmitting || errors.lookupCode}
+                    aria-label="Look up a Chapter"
                 >
                     <p className="hidden sm:block">Look up a Chapter</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 aspect-square fill-primary-content">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 aspect-square fill-primary-content" aria-hidden="true" focusable="false" >
                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                     </svg>
                 </button>
