b6f389e3d995a31828d11b0c5946eb7665741504
James <119804969+James-Sundby@users.noreply.github.com>
2025-03-14 10:29:57 -0600

    Adjusts storage callout

--- app/components/storageAlert.js
+++ app/components/storageAlert.js
@@ -21,20 +21,31 @@ export default function StorageAlert() {
     return (
         <>
             {showAlert && (
-                <div role="alert" className="card card-compact bg-base-300 max-w-xs shadow-lg rounded-lg border border-primary">
-                    <div className="card-body">
-                        <p className="">
-                            This site uses session storage to temporarily save your chapter data and paint scheme to your browser. This data is not shared or stored after your session ends.
-                        </p>
-                        <div className="card-actions justify-end">
-                            <button
-                                className="btn btn-sm btn-primary rounded-lg"
-                                onClick={handleDismiss}
-                            >
-                                Got it
-                            </button>
-                        </div>
-                    </div>
+                // <div role="alert" className="card card-sm bg-base-300 max-w-xs shadow-lg rounded-lg border border-primary">
+                //     <div className="card-body">
+                //         <p className="">
+                //             This site uses session storage to temporarily save your chapter data and paint scheme to your browser. This data is not shared or stored after your session ends.
+                //         </p>
+                //         <div className="card-actions justify-end">
+                //             <button
+                //                 className="btn btn-sm btn-primary rounded-lg"
+                //                 onClick={handleDismiss}
+                //             >
+                //                 Got it
+                //             </button>
+                //         </div>
+                //     </div>
+                // </div>
+                <div role="alert" className="alert alert-vertical max-w-xs text-left rounded-lg border border-primary">
+                    <p className="">
+                        This site uses session storage to temporarily save your chapter data and paint scheme to your browser. This data is not shared or stored after your session ends.
+                    </p>
+                    <button
+                        className="btn btn-primary rounded-lg ml-auto"
+                        onClick={handleDismiss}
+                    >
+                        Got it
+                    </button>
                 </div>
             )}
         </>
