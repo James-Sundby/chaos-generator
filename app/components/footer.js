1067365de1c7dee3676e8c6943ae946cffab7f6a
James <119804969+James-Sundby@users.noreply.github.com>
2025-04-15 19:33:27 -0600

    Adds CSM test file and preview page
    Also updates next

--- app/components/footer.js
+++ app/components/footer.js
@@ -2,28 +2,33 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4">
+    <footer className="footer footer-center p-4 text-xs text-neutral-content">
       <div>
-        <p>Based on line art by{" "}
+        <p>
+          Based on line art by{" "}
           <Link
             className="underline"
             href="https://www.deviantart.com/40kresources"
             target="_blank"
-            aria-label="Visit 40kResources' DeviantArt page, the creator from which the line art was adapted">
+            rel="noopener noreferrer"
+            aria-label="Visit 40kResources' DeviantArt page"
+            title="Visit 40kResources' DeviantArt page"
+          >
             40kResources
           </Link>
-          {" "}under{" "}
+          {" "}and{" "}
           <Link
             className="underline"
-            href="https://creativecommons.org/licenses/by/3.0/ "
+            href="https://www.deviantart.com/shawnpleil"
             target="_blank"
-            aria-label="Read the Creative Commons 3.0 license definition."
+            rel="noopener noreferrer"
+            aria-label="Visit ShawnPleil's DeviantArt page"
+            title="Visit ShawnPleil's DeviantArt page"
           >
-            CC 3.0
+            ShawnPleil
           </Link>
         </p>
       </div>
     </footer>
   );
 }
-
