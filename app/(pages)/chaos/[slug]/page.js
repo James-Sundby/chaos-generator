2c3f5ec5f0342fc1420e009af9963bb4443b549e
James <119804969+James-Sundby@users.noreply.github.com>
2025-04-18 15:33:56 -0600

    Change to svh for image height

--- app/(pages)/chaos/[slug]/page.js
+++ app/(pages)/chaos/[slug]/page.js
@@ -50,11 +50,11 @@ export default function Page() {
     const { warbandName, colors = [], slug } = chaosBand;
 
     return (
-        <main className="flex flex-1 flex-col gap-4 sm:gap-16 items-center justify-center p-4">
+        <main className="flex flex-1 flex-col gap-4 sm:gap-16 items-center justify-center px-4">
             <div id="trading-card" className="card bg-yellow-600 text-neutral w-full max-w-96 rounded-lg">
                 <div className="card-body p-2 m-2 bg-white rounded-lg">
                     <h1 className="card-title justify-center text-center">{warbandName}</h1>
-                    <div className="h-[50dvh] sm:h-auto">
+                    <div className="h-[50svh] sm:h-auto">
                         <ChaosMarine
                             body={colors[0]?.hex}
                             edge={colors[1]?.hex}
