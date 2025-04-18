9a758075a756940e0e03743b644368450aa5a042
James <119804969+James-Sundby@users.noreply.github.com>
2025-04-18 17:14:51 -0600

    Removes vertical centering on mobile

--- app/(pages)/open-paint/page.js
+++ app/(pages)/open-paint/page.js
@@ -33,8 +33,7 @@ export default function Home() {
     };
 
     return (
-        <main className="flex flex-1 flex-col justify-center items-center gap-4 px-4">
-
+        <main className="flex flex-1 flex-col justify-center items-center gap-4 px-4 pb-4">
             <aside className="w-full max-w-96 sm:max-w-[52rem] ">
                 <ImportWarband />
             </aside>
