77c3f7cd1458567a2f15fcd4293adc167c91efaf
James <119804969+James-Sundby@users.noreply.github.com>
2025-05-09 16:13:23 -0600

    Another attempt to fix og:card issues

--- app/(pages)/chapter/[slug]/page.js
+++ app/(pages)/chapter/[slug]/page.js
@@ -17,7 +17,7 @@ export async function generateMetadata({ params }) {
                 siteName: "Chapter Generator",
                 images: [
                     {
-                        url: "https://chapter-gen.jsundby.dev/card-image.jpg",
+                        url: "https://chapter-gen.jsundby.dev/card-image.jpeg",
                         width: 1200,
                         height: 630,
                         alt: "Paintbrushes in a jar. Text overlay: Stuck with primer? Generate a chapter and break the block.",
@@ -32,7 +32,7 @@ export async function generateMetadata({ params }) {
                 card: 'summary_large_image',
                 images: [
                     {
-                        url: "https://chapter-gen.jsundby.dev/card-image.jpg",
+                        url: "https://chapter-gen.jsundby.dev/card-image.jpeg",
                         alt: "Paintbrushes in a jar. Text overlay: Stuck with primer? Generate a chapter and break the block."
                     }
                 ],
