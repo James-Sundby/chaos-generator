44471c0d0d2cc91de1b3c593f2f5a627e5e43b9f
James <119804969+James-Sundby@users.noreply.github.com>
2026-06-29 13:27:23 -0600

    Removes old index files

--- utils/generateColourScheme.js
+++
@@ -1,6 +0,0 @@
-import "server-only";
-
-export { generateScheme as generateChapterScheme } from "@/lib/factions/chapter/scheme";
-export { generateScheme as generateWarbandScheme } from "@/lib/factions/chaos/scheme";
-export { generateScheme as generateEldarScheme } from "@/lib/factions/eldar/scheme";
-export { generateScheme as generateSistersScheme } from "@/lib/factions/sisters/scheme";
